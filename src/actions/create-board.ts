05d325a284c2f9d5c6cdda00ad79d36dc51caf79
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2024-03-16 01:14:36 +0530

    comment out unused code

--- src/actions/create-board.ts
+++ src/actions/create-board.ts
@@ -1,26 +1,26 @@
-"use server";
+// "use server";
 
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs/server";
-import { z } from "zod";
-import { boardFormSchema } from "./action-schema";
+// import { db } from "@/lib/db";
+// import { auth } from "@clerk/nextjs/server";
+// import { z } from "zod";
+// import { boardFormSchema } from "./action-schema";
 
-export async function createBoard(data: z.infer<typeof boardFormSchema>) {
-  const { orgId } = auth();
-  const { title, imageId } = boardFormSchema.parse(data);
+// export async function createBoard(data: z.infer<typeof boardFormSchema>) {
+//   const { orgId } = auth();
+//   const { title, imageId } = boardFormSchema.parse(data);
 
-  await db.taskApp_Board.create({
-    data: {
-      title: title,
-    orgId: orgId as string,
-      imageId: imageId as string,
-    },
-  });
-}
+//   await db.taskApp_Board.create({
+//     data: {
+//       title: title,
+//     orgId: orgId as string,
+//       imageId: imageId as string,
+//     },
+//   });
+// }
 
-export async function getBoards(orgId: string) {
-  const boards = await db.taskApp_Board.findMany({
-    where: { orgId: orgId },
-  });
-  return boards;
-}
+// export async function getBoards(orgId: string) {
+//   const boards = await db.taskApp_Board.findMany({
+//     where: { orgId: orgId },
+//   });
+//   return boards;
+// }
